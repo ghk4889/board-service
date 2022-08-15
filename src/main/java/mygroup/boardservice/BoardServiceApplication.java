c8478375d4cd753c554d9d4ae6a5cee8fca1a720
JongDeu <ghk4889@naver.com>
2022-08-16 01:37:31 +0900

    Docs & Feat: 문서 편집 & index.html 편집

--- src/main/java/mygroup/boardservice/BoardServiceApplication.java
+++ src/main/java/mygroup/boardservice/BoardServiceApplication.java
@@ -1,8 +1,17 @@
 package mygroup.boardservice;
 
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
+import javax.sql.DataSource;
+
+@MapperScan(basePackageClasses = BoardServiceApplication.class)
 @SpringBootApplication
 public class BoardServiceApplication {
 
@@ -10,4 +19,13 @@ public class BoardServiceApplication {
 		SpringApplication.run(BoardServiceApplication.class, args);
 	}
 
+	@Bean
+	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+		final SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+		sessionFactory.setDataSource(dataSource);
+		Resource configLocation = new PathMatchingResourcePatternResolver().getResource("classpath:mybatis-config.xml");
+
+		sessionFactory.setConfigLocation(configLocation);
+		return sessionFactory.getObject();
+	}
 }
