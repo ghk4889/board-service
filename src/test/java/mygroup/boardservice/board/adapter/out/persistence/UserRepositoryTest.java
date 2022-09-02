1c90e1215cc42b3234ca88ed92b2b8648c3969ec
JongDeu <ghk4889@naver.com>
2022-09-02 22:20:54 +0900

    [#8] add save
    - User 객체에 select용 생성자, insert용 생성자를 추가함.
    - UserMapper.xml에 save 쿼리를 추가함.
    - save 기능을 테스트함.

--- src/test/java/mygroup/boardservice/board/adapter/out/persistence/UserRepositoryTest.java
+++ src/test/java/mygroup/boardservice/board/adapter/out/persistence/UserRepositoryTest.java
@@ -2,9 +2,13 @@ package mygroup.boardservice.board.adapter.out.persistence;
 
 import lombok.extern.slf4j.Slf4j;
 import mygroup.boardservice.board.adapter.out.persistence.mapper.UserMapper;
+import mygroup.boardservice.board.domain.Grade;
+import mygroup.boardservice.board.domain.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.sql.Date;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -28,6 +32,12 @@ class UserRepositoryTest {
 
     @Test
     void save() {
+        User user = new User("test@gmail.com", "testname"
+                , new Date(System.currentTimeMillis()), Grade.VIP, "testpwd");
+
+        userMapper.save(user);
+
+        log.info( "insert 후 객체 id 값: " + user.getId());
     }
 
     @Test
